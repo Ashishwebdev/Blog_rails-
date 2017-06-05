410b390cc5fc8cb9a6c994e6d730cab10b3888ac
singhashish19031998@gmail.com <singhashish19031998@gmail.com>
2017-06-05 23:06:15 +0530

    Now you can Edit and Delete the posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,31 @@ render 'new'
 def show
 @post = Post.find(params[:id])
 end
+
+def edit
+  @post = Post.find(params[:id])
+end
+
+def update
+  @post = Post.find(params[:id])
+
+  if @post.update(post_params)
+ redirect_to @post
+  else
+ render 'edit'
+  end
+
+end
+
+
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+  redirect_to @post
+end
+
+
+
 private
   def post_params
     params.require( :posts).permit(:title, :body)
